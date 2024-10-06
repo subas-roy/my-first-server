346eb2cc81eebd8014cbe0597311c8ac921e9c5a
subas-roy <subasroy46@gmail.com>
2024-10-07 05:42:07 +0600

    Create get all data api and single phone api

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hellow from my first ever server.');
+  res.send('Hello from my first ever server.');
 })
 
-app.get('/data', (req, res) => {
-  res.send('Hellow from my first ever server DATA.');
+app.get('/phones', (req, res) => {
+  res.send(phones);
+})
+
+app.get('/phones/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log('I need data for id', id);
+  const phone = phones.find(phone => phone.id === id) || {};
+  res.send(phone);
 })
 
 app.listen(port, () => {
